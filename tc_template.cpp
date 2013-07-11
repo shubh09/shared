f4952d3d41dab323c1f7d2d7a850b901414699b3
shubham <shubh09@gmail.com>
2013-07-11 23:05:39 +0530

    Updated Netbeans template. Added topcoder template.

---
+++ tc_template.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <utility>
+#include <stack>
+#include <sstream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+
+using namespace std;
+
+#define FOR(i,a,b)				for (i=a;i<b;i++)
+#define s(n)					scanf("%d",&n)
+#define p(n)					printf("%d\n",n)
+#define sd(n)					int n;scanf("%d",&n)
+#define pb(n)                                   push_back(n)
+#define clr(a)                                  memset(a,0,sizeof(a))
+#define all(c)                                  (c).begin(),(c).end()
+#define tr(container,it)                        for (typeof(container.begin()) it=container.begin();it!=container.end();it++ )
+#define sz(a)                                   int((a).size())
+#define mp(a,b)                                 make_pair(a,b)
+
+#define INF                                     INT_MAX
+#define UINF                                    UINT_MAX
+#define INF_LL                                  LLONG_MAX
+#define UINF_LL                                 ULLONG_MAX
+
+typedef vector <int> vi;
+typedef vector <vi> vvi;
+typedef vector <string> vstr;
+typedef long long ll;
+
+class $CLASSNAME$ {
+	public:
+	$RC$ $METHODNAME$($METHODPARMS$)
+	{
+		int i,j;
+
+	}
+	$TESTCODE$
+};
+
+$BEGINCUT$
+int main()
+{
+	$CLASSNAME$ ___test; 
+	___test.run_test(-1); 
+} 
+$ENDCUT$
